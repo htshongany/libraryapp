3e52fa5ca0189200bccb176a0cae6af63c6edba4
htshongany <htshongany@gmail.com>
2023-04-20 20:05:12 -0700

    add [library-detail-view]

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index , get_pdf_detail
 
 app_name = 'library'
 
 urlpatterns = [
-    path('', index , name='index')
+    path('', index , name='index'),
+    path('detail/<str:slug>', get_pdf_detail , name='book-detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -7,4 +7,12 @@ def index(request):
     context = {
         "books":Book.objects.filter(published=True),
     }
-    return render(request , 'library/index.html', context) 
+    return render(request , 'library/index.html', context)
+
+
+def get_pdf_detail(request, slug):
+    context = {
+        "book": get_object_or_404(Book.objects.filter(published=True, slug=slug))
+    }
+
+    return render(request,"library/book-view.html" ,context)
