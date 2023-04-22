cfee2ec1b8193cb50c92b00baa8ca0d64bdc0158
htshongany <htshongany@gmail.com>
2023-04-21 21:28:04 -0700

    fix [media-url and media-root for production and mv to library:urls]

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index , get_pdf_detail , get_pdf_preview
 
 app_name = 'library'
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', index , name='index'),
     path('detail/<str:slug>', get_pdf_detail , name='book-detail'),
     path('preview/<str:slug>', get_pdf_preview , name='book-preview'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
