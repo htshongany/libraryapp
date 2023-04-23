7bc1a0271d1ae3b9e5e76a7a6c862e2887d1a3ae
htshongany <htshongany@gmail.com>
2023-04-23 14:16:06 -0700

    add [library-search-books-by-title]

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index , get_pdf_detail , get_pdf_preview
+from .views import index , get_pdf_detail , get_pdf_preview , search_by_title
 
 app_name = 'library'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', index , name='index'),
     path('detail/<str:slug>', get_pdf_detail , name='book-detail'),
     path('preview/<str:slug>', get_pdf_preview , name='book-preview'),
+    path('search/results/', search_by_title , name='search-results')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- library/views.py
+++ library/views.py
@@ -22,3 +22,21 @@ def get_pdf_preview(request, slug):
         "book": get_object_or_404(Book.objects.filter(published=True, slug=slug))
     }
     return render(request,"library/book-preview.html" ,context)
+
+
+
+
+def search_by_title(request):
+    object_list = _search_book(request)
+    context = {
+        'object_list':object_list,
+        }
+    
+    return render(request,"library/search-results.html" ,context)
+
+def _search_book(request):
+    if request.method == "POST":
+        # print(request.method.POST['search'])
+        search = request.POST.get("search")
+        object_list = Book.objects.filter(title__contains=search , published=True)
+    return object_list
