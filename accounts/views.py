d47b60e639e7e0debb451e1c7b56371b40e4eefe
htshongany <htshongany@gmail.com>
2023-05-02 17:49:51 -0700

    add [accounts-login-view]

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
+
+def login_view(request):
+
+    if request.method =='POST': 
+        # Get the username and password from the request
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Try to authenticate the user
+        user = authenticate(username=username, password=password)
+
+        # If the user is authenticated, log them in and redirect to the home page
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            return redirect('library:index')
+
+        else:
+            messages.error(request, 'The username or password is incorrect.')
+            return redirect('accounts:login')
+
+    return render(request, 'accounts/login.html')
